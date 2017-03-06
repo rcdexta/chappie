af206364cca893b61e905d025d08b2950c3b1f3c
RC <rc.chandru@gmail.com>
2017-03-06 07:07:26 +0530

    Adding more responses and tweaking help commands

--- handlers/responder.js
+++ handlers/responder.js
@@ -26,12 +26,12 @@ module.exports = {
       CalendarHandler.wfh(controller, bot, message);
     });
 
-    controller.hears(['who\\s+is\\s+wfh', '.*who.*wfh.*', '.*who.*working\\s+from\\s+home.*'], 'direct_message,direct_mention,mention', function (bot, message) {
+    controller.hears(['who\\s+are\\s+wfh', '.*who.*wfh.*', '.*who.*working\\s+from\\s+home.*'], 'direct_message,direct_mention,mention', function (bot, message) {
       console.log('Responding to <who is wfh>');
       CalendarHandler.stats(controller, bot, message);
     });
 
-    controller.hears(['who\\s+is\\s+coming', '.*who.*coming.*', '.*who.*coming\\s+to\\s+office.*'], 'direct_message,direct_mention,mention', function (bot, message) {
+    controller.hears(['who\\s+are\\s+coming', '.*who.*coming.*', '.*who.*coming\\s+to\\s+office.*'], 'direct_message,direct_mention,mention', function (bot, message) {
       console.log('Responding to <who is wfh>');
       CalendarHandler.stats(controller, bot, message);
     });
@@ -55,7 +55,7 @@ module.exports = {
         UserHandler.lookup(controller, bot, message).then(function (user) {
           bot.reply(message,
             "Chappie can understand the following questions! Ask anything else at your own risk :smiling_imp: \n `wfh` \n `wfh today`" +
-            "\n `wfh tomorrow` \n `office today`  \n `office tomorrow`  \n`who is coming` \n `who is wfh`");
+            "\n `wfh tomorrow` \n `office today`  \n `office tomorrow`  \n`who are coming` \n `who are wfh`");
         });
       });
 
--- handlers/talker.js
+++ handlers/talker.js
@@ -2,29 +2,32 @@
 var sprintf = require("sprintf-js").sprintf;
 
 Array.prototype.shuffle = function () {
-    return this[Math.floor(Math.random() * this.length)];
+  return this[Math.floor(Math.random() * this.length)];
 };
 
 module.exports = {
 
-    wfh: ["Long commutes make you fat, stressed, and miserable! You chose wisely! %s!",
-        "Commuting doesn’t do a body good. It isn’t good for the environment either. Smart thinking %s!",
-        "Work from home and be your own boss. Have a power cut free day %s!",
-        "After all, I realised you cannot teleport like me! :stuck_out_tongue: Good day %s..",
-        "Hope there is no scheduled power shutdown at your place! :wink: Just kidding... Take care",
-        "Good for you %s. Here have a comic digression: http://dilbert.com/strip/2010-05-02",
-        "Hope you have a calm and commute free day %s. I am serious! http://dilbert.com/strip/2014-07-10"],
+  wfh: ["Long commutes make you fat, stressed, and miserable! You chose wisely! %s!",
+    "Commuting doesn’t do a body good. It isn’t good for the environment either. Smart thinking %s!",
+    "Work from home and be your own boss. Have a power cut free day %s!",
+    "After all, I realised you cannot teleport like me! :stuck_out_tongue: Good day %s..",
+    "Hope there is no scheduled power shutdown at your place! :wink: Just kidding... Take care",
+    "Good for you %s. Here have a comic digression: http://dilbert.com/strip/2010-05-02",
+    "Hope you have a calm and commute free day %s. I am serious! http://dilbert.com/strip/2014-07-10",
+    "Well I hope you don't mean this! ;) http://bit.ly/2n41XLG"],
 
-    office: ["Great %s! High speed wifi and low carb sundal awaits you!",
-        "See you %s! I guess you love our office space! http://dilbert.com/strip/2015-10-24",
-        "Great! I am planning to order a new office chair for you: https://xkcd.com/1329/"],
+  office: ["Great %s! High speed wifi and low carb sundal awaits you!",
+    "See you %s! I guess you love our office space! http://dilbert.com/strip/2015-10-24",
+    "Nice %s :) I am planning to order a new office chair for you: https://xkcd.com/1329/",
+    "You chose wisely!",
+    "Please start early %s else see you for lunch! Come safe..."],
 
-    sayWfh: function (name) {
-        return sprintf(this.wfh.shuffle(), name);
-    },
+  sayWfh: function (name) {
+    return sprintf(this.wfh.shuffle(), name);
+  },
 
-    sayOffice: function (name) {
-        return sprintf(this.office.shuffle(), name);
-    }
+  sayOffice: function (name) {
+    return sprintf(this.office.shuffle(), name);
+  }
 
 };
