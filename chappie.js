1a25b121b0b8b9d537514abb073ffc01a2ecf424
RC <rc.chandru@gmail.com>
2017-08-10 07:46:35 +0530

    For auto-responder, should format date as key before storing in calendar

--- chappie.js
+++ chappie.js
@@ -62,7 +62,7 @@ controller.setupWebserver(process.env.port, function (err, webserver) {
 
     if (payload.callback_id === 'wfh_interaction') {
       const location = payload.actions[0].name === 'yes' ? 'office' : 'home';
-      let key = Time.tomorrowForEpochTime(payload.message_ts);
+      let key = Time.formatYYYYMMDD(Time.tomorrowForEpochTime(payload.message_ts));
       CalendarHandler.saveToCalendar(controller, null, user, key, location);
     }
 
